9ae71eaabc5375b4d261a78bab60f76b2278c799
firozt <firozt03@gmail.com>
2026-03-13 17:35:12 +0000

    added line wrapping for markdown files

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,6 +2,15 @@
 local M = {}
 
 function M.setup()
+  -- line wrap for .md files
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'markdown',
+    callback = function()
+      vim.opt_local.wrap = true
+      vim.opt_local.linebreak = true -- wrap at word boundaries not mid-word
+    end,
+  })
+
   -- Move selected block down
   vim.keymap.set('v', 'J', ":<C-u>'<,'>m '>+1<CR>gv=gv")
 
